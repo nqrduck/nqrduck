a05e63c137337cf2eb2c0f97f454cb04d5c1234a
jupfi <github@jupfi.me>
2023-07-29 18:07:56 +0200

    Added serializer for saving of decimals.

---
+++ src/nqrduck/helpers/serializer.py
@@ -0,0 +1,23 @@
+import json
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    """This class is used to encode decimal values to json."""
+    def default(self, obj):
+        """This method is called by the json encoder to encode the object.
+        
+        Args:
+            obj (object): The object to be encoded
+            
+        Returns:
+            str: The encoded object
+        
+        Examples:
+            >>> import json
+            >>> from decimal import Decimal
+            >>> json.dumps(Decimal("1.0"), cls=DecimalEncoder)
+        """
+        
+        if isinstance(obj, Decimal):
+            return str(obj)  # or float(obj) if you want to keep it as a number
+        return super(DecimalEncoder, self).default(obj)
