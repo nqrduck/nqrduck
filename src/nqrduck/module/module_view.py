a1cea092a2b706a0d8ae44aa88f45a199480d456
jupfi <github@jupfi.me>
2023-12-16 09:54:55 +0100

    Added FileManager to ModuleView.

--- src/nqrduck/module/module_view.py
+++ src/nqrduck/module/module_view.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QFileDialog
 from PyQt6.QtCore import pyqtSignal, QObject
 from nqrduck.module.module import Module
 
@@ -26,3 +26,46 @@ class ModuleView(QWidget):
     @module.setter
     def module(self, value):
         self._module = value
+
+    class QFileManager:
+        """This class provides methods for opening and saving files."""
+        def __init__(self, extenstion, parent=None):
+            self.extension = extenstion
+            self.parent = parent
+
+        def loadFileDialog(self) -> str:
+            """Opens a file dialog for the user to select a file to open.
+            
+            Returns:
+                str: The path of the file selected by the user.
+            """
+            extension_name = self.extension.upper()
+            fileName, _ = QFileDialog.getOpenFileName(self.parent,
+                                                    "QFileManager - Open File",
+                                                    "",
+                                                    "%s Files (*.%s);;All Files (*)" % (extension_name, self.extension),
+                                                    options=QFileDialog.Option.DontUseNativeDialog)
+            if fileName:
+                return fileName
+            else:
+                return None
+
+        def saveFileDialog(self) -> str:
+            """Opens a file dialog for the user to select a file to save.
+            
+            Returns:
+                str: The path of the file selected by the user.
+            """
+            extension_name = self.extension.upper()
+            fileName, _ = QFileDialog.getSaveFileName(self.parent,
+                                                    "QFileManager - Save File",
+                                                    "",
+                                                    "%s Files (*.%s);;All Files (*)" % (extension_name, self.extension),
+                                                    options=QFileDialog.Option.DontUseNativeDialog)
+            if fileName:
+                # Append the .quack extension if not present
+                if not fileName.endswith(". %s" % self.extension):
+                    fileName += ".%s" % self.extension
+                return fileName
+            else:
+                return None
