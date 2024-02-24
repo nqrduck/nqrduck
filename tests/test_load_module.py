4f6338e4e04a2b8ffc5587dddb7ab6cc6db877a4
jupfi <github@jupfi.me>
2024-02-24 12:25:45 +0100

    Added load module test.

---
+++ tests/test_load_module.py
@@ -0,0 +1,36 @@
+# Tests that loads a module from the nqrduck entry points
+import pytest
+from PyQt6.QtWidgets import QApplication
+from nqrduck.core.main_model import MainModel
+from nqrduck.core.main_controller import MainController
+
+def test_load_module():
+    main_view = StubMainView()
+    
+    main_model = MainModel()
+    main_controller = MainController(main_model)
+
+    main_controller.load_modules(main_view)
+    main_model.active_module = main_model.loaded_modules[list(main_model.loaded_modules.keys())[0]]
+    assert main_model.active_module is not None
+    assert main_model.loaded_modules is not None
+
+
+class StubMainView(QApplication):
+    """This is only a stub for the tests"""
+    def __init__(self):
+        super().__init__([])
+ 
+
+    def on_module_widget_added(self, widget):
+        """ This is only a stub for the tests"""
+        pass
+
+    def on_menu_bar_item_added(self, menu_item):
+        """ This is only a stub for the tests"""
+        pass
+
+
+if __name__ == "__main__":
+    test_load_module()
+    print("load_module.py: All tests passed")
